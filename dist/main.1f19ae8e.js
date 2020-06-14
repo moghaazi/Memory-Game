dc2393bffab78e4d3eadd48dafd5b625ef345757
moghazi <moghaazi@users.noreply.github.com>
2020-06-14 22:33:57 +0200

    fdf

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -215,7 +215,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7004" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11106" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
